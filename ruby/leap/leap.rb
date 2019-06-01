c5985190bca592f3d7d6ea8877610e87a58f8396
Murilo Vasconcelos <muriloime@gmail.com>
2019-06-01 11:13:49 -0300

    fix

--- ruby/leap/leap.rb
+++ ruby/leap/leap.rb
@@ -2,6 +2,6 @@
 
 module Year
   def self.leap?(year)
-    (year % 4).zero? && (!(year % 100).zero? || (year % 400).zero?
+    (year % 4).zero? && (!(year % 100).zero? || (year % 400).zero?)
   end
 end
