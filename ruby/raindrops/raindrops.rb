0b36dd1e12a81bb9bdb69bf1398c461966eef589
Murilo Vasconcelos <muriloime@gmail.com>
2019-06-06 18:01:29 -0300

    josh's suggestions

--- ruby/raindrops/raindrops.rb
+++ ruby/raindrops/raindrops.rb
@@ -1,12 +1,12 @@
 module Raindrops
-  def self.convert(number)
-    factor_to_output = { 3 => 'Pling',
-                         5 => 'Plang',
-                         7 => 'Plong' }.freeze
+  FACTOR_TO_OUTPUT = { 3 => 'Pling',
+                       5 => 'Plang',
+                       7 => 'Plong' }.freeze
 
-    converted_number = factor_to_output.select do |factor, _output|
-      (number % factor).zero?
-    end.map(&:last).join
+  def self.convert(number)
+    converted_number = FACTOR_TO_OUTPUT.map do |factor, output|
+      output if (number % factor).zero?
+    end.join
 
     converted_number.empty? ? number.to_s : converted_number
   end
