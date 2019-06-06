cad1d8fc03deaa56d7ee01808c60fa64f825408e
Murilo Vasconcelos <muriloime@gmail.com>
2019-06-06 06:18:11 -0300

    create exception class

--- ruby/hamming/hamming.rb
+++ ruby/hamming/hamming.rb
@@ -1,10 +1,14 @@
 module Hamming
   def self.compute(strand1, strand2)
-    unless strand1.size == strand2.size
-      raise ArgumentError, 'Strand lengths must be identical'
-    end
+    raise StrandsLengthError unless strand1.length == strand2.length
 
     nucleotides = strand1.chars.zip(strand2.chars)
     nucleotides.count { |n1, n2| n1 != n2 }
+  end
+end
+
+class StrandsLengthError < ArgumentError
+  def initialize(message = 'Strand lengths must be identical')
+    super(message)
   end
 end
