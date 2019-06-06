5a2c6633b2a0166ff2425c758a66fe21ce027363
Murilo Vasconcelos <muriloime@gmail.com>
2019-06-06 18:26:03 -0300

    improve from other people solutions

--- ruby/nucleotide-count/nucleotide_count.rb
+++ ruby/nucleotide-count/nucleotide_count.rb
@@ -2,25 +2,27 @@ class Nucleotide
   TYPES = %w[A T C G].freeze
 
   def initialize(dna)
+    raise InvalidDna unless valid?(dna)
+
     @dna = dna
   end
 
-  def self.valid?(dna)
-    (dna.chars.uniq - TYPES).empty?
-  end
-
   def self.from_dna(dna)
-    raise InvalidDna unless valid?(dna)
-
     new(dna)
   end
 
   def count(type)
-    @dna.chars.count { |nucleotide| nucleotide == type }
+    @dna.chars.count(type)
   end
 
   def histogram
     Hash[TYPES.map { |type| [type, count(type)] }]
+  end
+
+  private
+
+  def valid?(dna)
+    (dna.chars.uniq - TYPES).empty?
   end
 end
 
