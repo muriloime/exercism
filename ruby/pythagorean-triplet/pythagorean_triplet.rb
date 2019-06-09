8198b03e0364896b6683c5f9c41abb34362b1f3e
Murilo Vasconcelos <muriloime@gmail.com>
2019-06-09 08:01:07 -0300

    improve pythagorean with josh suggestions

--- ruby/pythagorean-triplet/pythagorean_triplet.rb
+++ ruby/pythagorean-triplet/pythagorean_triplet.rb
@@ -1,39 +1,50 @@
 # frozen_string_literal: true
 
 class Triplet
-  def initialize(a, b, c)
-    @sides = [a, b, c].sort
+  def initialize(*sides)
+    raise IncorrectTripletSizeError if sides.count != 3
+
+    @sides = sides
   end
 
   def sum
-    @sides.sum
+    sides.sum
   end
 
   def product
-    @sides.reduce(:*)
+    sides.reduce(:*)
   end
 
   def pythagorean?
-    (@sides[0]**2 + @sides[1]**2 - @sides[2]**2).zero?
-  end
-
-  def self.possible_triplets(factor_range)
-    factor_range
-      .combination(3)
-      .map { |triplet| Triplet.new(*triplet) }
-  end
-
-  def self.filter(candidates, sum, product)
-    candidates.select do |triplet|
-      (!sum || triplet.sum == sum) &&
-        (!product || triplet.product == product)
-    end
+    a, b, c = sides
+    (a**2 + b**2 - c**2).zero?
   end
 
   def self.where(min_factor: 1, max_factor:, sum: nil, product: nil)
-    all_triplets = possible_triplets((min_factor..max_factor).to_a)
-    filter(all_triplets,
-           sum,
-           product).select(&:pythagorean?)
+    possible_triplets(min_factor..max_factor)
+      .select { |triplet| triplet.satisfy?(sum, product) }
+      .select(&:pythagorean?)
+  end
+
+  def satisfy?(sum, product)
+    (sum.nil? || self.sum == sum) &&
+      (product.nil? || self.product == product)
+  end
+
+  private
+
+  attr_reader :sides
+  class << self
+    def possible_triplets(factor_range)
+      factor_range.to_a
+                  .combination(3)
+                  .map { |triplet| Triplet.new(*triplet) }
+    end
+  end
+end
+
+class IncorrectTripletSizeError < ArgumentError
+  def initialize
+    super('Triplet must have 3 sides')
   end
 end
