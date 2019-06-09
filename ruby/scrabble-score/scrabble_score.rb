1eae956411d3ef303fa35fe33b94b4ce01ed6b1b
Murilo Vasconcelos <muriloime@gmail.com>
2019-06-09 19:46:34 -0300

    update scrabble

--- ruby/scrabble-score/scrabble_score.rb
+++ ruby/scrabble-score/scrabble_score.rb
@@ -1,24 +1,41 @@
 class Scrabble
-  POINTS_TABLE = [[%w[A E I O U L N R S T], 1],
-                  [%w[D G], 2],
-                  [%w[B C M P], 3],
-                  [%w[F H V W Y], 4],
-                  [%w[K], 5],
-                  [%w[J X], 8],
-                  [%w[Q Z], 10]].freeze
-
-  LETTER_TO_POINTS = Hash[
-                      POINTS_TABLE.flat_map do |letters, score|
-                        letters.map { |l| [l, score] }
-                      end
-                    ]
+  LETTER_TO_POINTS = { 'A' => 1,
+                       'E' => 1,
+                       'I' => 1,
+                       'O' => 1,
+                       'U' => 1,
+                       'L' => 1,
+                       'N' => 1,
+                       'R' => 1,
+                       'S' => 1,
+                       'T' => 1,
+                       'D' => 2,
+                       'G' => 2,
+                       'B' => 3,
+                       'C' => 3,
+                       'M' => 3,
+                       'P' => 3,
+                       'F' => 4,
+                       'H' => 4,
+                       'V' => 4,
+                       'W' => 4,
+                       'Y' => 4,
+                       'K' => 5,
+                       'J' => 8,
+                       'X' => 8,
+                       'Q' => 10,
+                       'Z' => 10 }.freeze
 
   def initialize(word)
-    @word = word.to_s
+    @word = word.nil? ? '' : word
   end
 
   def score
-    @word.upcase.scan(/\w/).map(&LETTER_TO_POINTS).sum
+    tiles.sum(&LETTER_TO_POINTS)
+  end
+
+  def tiles
+    @word.upcase.scan(/\w/)
   end
 
   def self.score(word)
