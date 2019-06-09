546cfc3a8864a689aa423aa1210e44d375ddb4eb
Murilo Vasconcelos <muriloime@gmail.com>
2019-06-09 19:35:33 -0300

    downto

--- ruby/beer-song/beer_song.rb
+++ ruby/beer-song/beer_song.rb
@@ -1,5 +1,5 @@
 module BeerSong
-  def self.accord(n, capitalize = false, _paragraph = false)
+  def self.pluralize(n, capitalize = false)
     if n > 1
       "#{n} bottles"
     elsif n == 1
@@ -12,16 +12,16 @@ module BeerSong
   def self.part(n, paragraph)
     (paragraph ? ' ' : '') +
       if n > 0
-        "#{accord(n, true)} of beer on the wall, #{accord(n)} of beer.\n" \
-          "Take #{n == 1 ? 'it' : 'one'} down and pass it around, #{accord(n - 1)} of beer on the wall.\n"
+        "#{pluralize(n, true)} of beer on the wall, #{pluralize(n)} of beer.\n" \
+          "Take #{n == 1 ? 'it' : 'one'} down and pass it around, #{pluralize(n - 1)} of beer on the wall.\n"
       else
         "No more bottles of beer on the wall, no more bottles of beer.\n" \
           "Go to the store and buy some more, 99 bottles of beer on the wall.\n"
       end
   end
 
-  def self.recite(start, n)
-    (start - n + 1..start).to_a.reverse.map do |i|
+  def self.recite(start, verses)
+    start.downto(start - verses + 1).map do |i|
       part(i, i != start)
     end.join
   end
