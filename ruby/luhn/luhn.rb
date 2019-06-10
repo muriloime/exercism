5121c5435548a0feb27424d6be8e8eaa84ee3070
Murilo Vasconcelos <muriloime@gmail.com>
2019-06-09 22:26:35 -0300

    update luhn

--- ruby/luhn/luhn.rb
+++ ruby/luhn/luhn.rb
@@ -1,41 +1,50 @@
 class Luhn
-  def initialize(string)
-    @string = string
+  def initialize(id_number)
+    @id_number = id_number.delete(' ')
   end
 
   def digits
-    @string.scan(/\d/).map(&:to_i)
+    @id_number.scan(/\d/).map(&:to_i)
   end
 
-  def compute(digit, to_double)
-    if to_double
-      (digit * 2).then { |x| x > 9 ? x - 9 : x }
-    else
-      digit
-    end
+  def compute_odd(digit)
+    (digit * 2).then { |x| x > 9 ? x - 9 : x }
   end
 
-  def invalid_characters?
-    @string.gsub(/[\d ]/, '').empty?
+  def only_valid_characters?
+    !@id_number.match(/\D/)
   end
 
   def short_string?
-    @string.gsub(' ', '').size <= 1
+    @id_number.size <= 1
+  end
+
+  def split_digits
+    even_list.reverse.each_slice(2).to_a.transpose
   end
 
   def checksum
-    digits.reverse.each_with_index.sum do |digit, idx|
-      compute(digit, idx.odd?)
-    end
+    even_indices, odd_indices = split_digits
+    odd_indices.sum { |x| compute_odd(x) } + even_indices.sum
   end
 
   def valid?
-    return false unless invalid_characters? && !short_string?
+    return false unless only_valid_characters? && !short_string?
 
     (checksum % 10).zero?
   end
 
-  def self.valid?(string)
-    new(string).valid?
+  def self.valid?(id_number)
+    new(id_number).valid?
+  end
+
+  private
+
+  def even_list
+    if digits.count.odd?
+      [0] + digits
+    else
+      digits
+    end
   end
 end
