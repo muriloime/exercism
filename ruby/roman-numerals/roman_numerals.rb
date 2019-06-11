e5032db5ca56abca8e60d8bfa2d6169aa758bf17
Murilo Vasconcelos <muriloime@gmail.com>
2019-06-11 20:24:48 -0300

    less code

--- ruby/roman-numerals/roman_numerals.rb
+++ ruby/roman-numerals/roman_numerals.rb
@@ -1,26 +1,24 @@
 class Integer
+  ROMAN_TABLE = [['M', 1000],
+                 ['CM', 900],
+                 ['D', 500],
+                 ['CD', 400],
+                 ['C', 100],
+                 ['XC', 90],
+                 ['L', 50],
+                 ['XL', 40],
+                 ['X', 10],
+                 ['IX', 9],
+                 ['V', 5],
+                 ['IV', 4],
+                 ['I', 1]].freeze
+
   def to_roman
     x = to_i
-    return '' if x.zero?
-    return 'M' + (x - 1000).to_roman if x >= 1000
-    return 'CM' + (x - 900).to_roman if x >= 900
-    return 'D' + (x - 500).to_roman if x >= 500
-    return 'CD' + (x - 400).to_roman if x >= 400
-    return 'C' + (x - 100).to_roman if x >= 100
-    return 'XC' + (x - 90).to_roman if x >= 90
-    return 'L' + (x - 50).to_roman if x >= 50
-    return 'XL' + (x - 40).to_roman if x >= 40
-    return 'X' + (x - 10).to_roman if x >= 10
-    return 'IX' if x == 9
-    return 5.to_roman + (x - 5).to_roman if x > 5
 
-    case x
-    when 1, 2, 3
-      'I' * x
-    when 4
-      'IV'
-    when 5
-      'V'
+    ROMAN_TABLE.each do |roman, int|
+      return roman + (x - int).to_roman if x >= int
     end
+    ''
   end
 end
