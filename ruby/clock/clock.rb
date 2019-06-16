34127e551e123cc5fcdb6d6f4a5f01befe89edb2
Murilo Vasconcelos <muriloime@gmail.com>
2019-06-15 21:28:37 -0300

    ik's improvements

--- ruby/clock/clock.rb
+++ ruby/clock/clock.rb
@@ -1,47 +1,34 @@
 class Clock
-  attr_reader :minutes_passed
-  MINUTES_IN_DAY = 24 * 60
   MINUTES_IN_HOUR = 60
+  MINUTES_IN_DAY = 24 * MINUTES_IN_HOUR
 
   def initialize(hour: 0, minute: 0)
-    @minutes_passed = Clock.in_minutes(hour: hour, minute: minute)
+    @minutes_passed = (minute + hour * MINUTES_IN_HOUR) % MINUTES_IN_DAY
   end
 
   def to_s
-    format('%02d:%02d', hour, minute)
+    format('%02d:%02d', *(minutes_passed.divmod MINUTES_IN_HOUR))
   end
 
   def +(other)
-    Clock.new(hour: 0, minute: minutes_passed + other.minutes_passed)
+    self.class.new(hour: 0, minute: minutes_passed + other.minutes_passed)
   end
 
   def -(other)
-    Clock.new(hour: 0, minute: minutes_passed - other.minutes_passed)
-  end
-
-  def ==(other)
-    minutes_passed == other.minutes_passed
-  end
-
-  def eql?(other)
-    self == other
+    self.class.new(hour: 0, minute: minutes_passed - other.minutes_passed)
   end
 
   def hash
     value.hash
   end
 
+  def ==(other)
+    minutes_passed == other.minutes_passed
+  end
+
+  alias eql? ==
+
   protected
 
-  def self.in_minutes(hour:, minute:)
-    (minute + hour * MINUTES_IN_HOUR) % MINUTES_IN_DAY
-  end
-
-  def hour
-    @minutes_passed / MINUTES_IN_HOUR
-  end
-
-  def minute
-    @minutes_passed % MINUTES_IN_HOUR
-  end
+  attr_reader :minutes_passed
 end
