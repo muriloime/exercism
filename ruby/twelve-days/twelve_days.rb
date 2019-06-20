085cbf6cac9d5d7458dc870fef5c1744f5224cda
Murilo Vasconcelos <muriloime@gmail.com>
2019-06-20 09:32:18 -0300

    simplify twelve days

--- ruby/twelve-days/twelve_days.rb
+++ ruby/twelve-days/twelve_days.rb
@@ -1,34 +1,31 @@
 module TwelveDays
-  GIFTS = ['and a Partridge in a Pear Tree',
-           'two Turtle Doves',
-           'three French Hens',
-           'four Calling Birds',
-           'five Gold Rings',
-           'six Geese-a-Laying',
-           'seven Swans-a-Swimming',
-           'eight Maids-a-Milking',
-           'nine Ladies Dancing',
-           'ten Lords-a-Leaping',
-           'eleven Pipers Piping',
-           'twelve Drummers Drumming'].freeze
+  GIFTS = ['a Partridge in a Pear Tree',
+           'two Turtle Doves, and',
+           'three French Hens,',
+           'four Calling Birds,',
+           'five Gold Rings,',
+           'six Geese-a-Laying,',
+           'seven Swans-a-Swimming,',
+           'eight Maids-a-Milking,',
+           'nine Ladies Dancing,',
+           'ten Lords-a-Leaping,',
+           'eleven Pipers Piping,',
+           'twelve Drummers Drumming,'].freeze
   ORDINALS = %w[first second third fourth fifth sixth seventh
                 eighth ninth tenth eleventh twelfth].freeze
 
-  module_function
-
-  def song
-    ORDINALS.map.with_index do |ordinal, i|
-      sentence(ordinal, list(GIFTS[0..i]))
+  def self.song
+    (0..ORDINALS.count - 1).map do |i|
+      verse(i)
     end.join("\n")
   end
 
-  def sentence(ordinal, gift_list)
-    "On the #{ordinal} day of Christmas my true love gave to me: #{gift_list}.\n"
-  end
+  private
 
-  def list(gifts)
-    return gifts[0].gsub(/\Aand /, '') if gifts.count == 1
-
-    gifts.reverse.join(', ')
+  class << self
+    def verse(n)
+      gift_list = GIFTS[0..n].reverse.join(' ')
+      "On the #{ORDINALS[n]} day of Christmas my true love gave to me: #{gift_list}.\n"
+    end
   end
 end
