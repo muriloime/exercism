ad0200ba9e703b6bb15768c38806748c108c2005
Murilo Vasconcelos <muriloime@gmail.com>
2019-07-08 20:31:22 -0300

    TableFormatter

--- ruby/tournament/tournament.rb
+++ ruby/tournament/tournament.rb
@@ -1,6 +1,7 @@
 class Tournament
-  def initialize(input)
+  def initialize(input, formatter = TableFormatter)
     @match_history = MatchHistory.new
+    @formatter = formatter
     parse(input)
   end
 
@@ -12,7 +13,7 @@ class Tournament
   end
 
   def tally
-    @match_history.tally
+    @formatter.display(@match_history.scores)
   end
 
   def self.tally(input)
@@ -21,24 +22,52 @@ class Tournament
 end
 
 class MatchHistory
+  attr_reader :scores
+
   RESULT = { 'win' => 1,
              'loss' => -1,
              'draw' => 0 }.freeze
+
   def initialize
     @scores = Hash.new { |hash, key| hash[key] = Results.new }
-  end
-
-  def tally
-    [Results.header, *sorted_scores.map { |k, v| v.tally(k) }].map { |row| row + "\n" }.join
-  end
-
-  def sorted_scores
-    @scores.sort_by { |k, x| [-x.p, k] }
   end
 
   def add_match(home, away, result)
     @scores[home].add(RESULT[result])
     @scores[away].add(RESULT[result] * -1)
+  end
+end
+
+class TableFormatter
+  def self.display(scores)
+    [header, *sorted_scores(scores).map { |k, v| show(v, k) }].join
+  end
+
+  def self.sorted_scores(scores)
+    scores.sort_by { |k, x| [-x.p, k] }
+  end
+
+  def self.header
+    RowFormatter.display(%w[Team MP W D L P])
+  end
+
+  def self.show(result, team)
+    RowFormatter.display([team, result.mp, result.w, result.d, result.l, result.p])
+  end
+end
+
+module RowFormatter
+  def self.display(cells)
+    format = [[30, false]] + [[2, true]] * (cells.size - 1)
+    cells.zip(format).map do |c|
+      CellFormatter.display(*c.flatten)
+    end.join(' | ') + "\n"
+  end
+end
+
+module CellFormatter
+  def self.display(content, size, align_right = true)
+    format("%#{align_right ? '' : '-'}#{size}.#{size}s", content).to_s
   end
 end
 
@@ -66,28 +95,5 @@ class Results
     elsif result.zero?
       @d += 1
     end
-  end
-
-  def tally(team)
-    Row.display([team, mp, w, d, l, p])
-  end
-
-  def self.header
-    Row.display(%w[Team MP W D L P])
-  end
-end
-
-module Row
-  def self.display(cells)
-    format = [[30, false]] + [[2, true]] * (cells.size - 1)
-    cells.zip(format).map do |c|
-      Cell.display(*c.flatten)
-    end.join(' | ')
-  end
-end
-
-module Cell
-  def self.display(content, size, align_right = true)
-    format("%#{align_right ? '' : '-'}#{size}.#{size}s", content).to_s
   end
 end
