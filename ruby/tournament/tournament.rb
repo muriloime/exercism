312cf6f8e164f7a64367a0e7086b87ceeec6ee0d
Murilo Vasconcelos <muriloime@gmail.com>
2019-07-10 20:52:29 -0300

    remove results , result

--- ruby/tournament/tournament.rb
+++ ruby/tournament/tournament.rb
@@ -3,109 +3,80 @@ class Tournament
     MatchHistory.new.tap do |match_history|
       input.split("\n").each do |match|
         home, away, result = match.split(';')
-        match_history.add_match(home, away, Result.new(result))
+        match_history.add_match(home, away, result.to_sym)
       end
     end
   end
 
   def self.tally(input)
     parse(input).then do |match_history|
-      TableFormatter.new(match_history).display
+      TableFormatter.new(match_history.teams.values).display
     end
   end
 end
 
 class MatchHistory
-  attr_reader :scores
+  attr_reader :teams
 
-  def initialize
-    @scores = Hash.new { |hash, key| hash[key] = Results.new(key) }
-  end
-
-  def add_match(home, away, result)
-    @scores[home].add(result)
-    @scores[away].add(result.inverse)
-  end
-end
-
-class TableFormatter
-  attr_reader :scores
-
-  ROW_FORMATTER = "%-30s | %2s | %2s | %2s | %2s | %2s\n".freeze
-  HEADER = "Team                           | MP |  W |  D |  L |  P\n".freeze
-
-  def initialize(match_history)
-    @scores = match_history.scores
-  end
-
-  def display
-    [HEADER, *sorted_scores.map { |_, v| show(v) }].join
-  end
-
-  def sorted_scores
-    scores.sort_by { |_, result| result }
-  end
-
-  def show(result)
-    format(ROW_FORMATTER, *result.row_list).to_s
-  end
-end
-
-class Result
   INVERSE = { win: :loss,
               loss: :win,
               draw: :draw }.freeze
 
-  def initialize(value)
-    @value = value.to_sym
+  def initialize
+    @teams = Hash.new { |hash, key| hash[key] = Team.new(key) }
   end
 
-  def inverse
-    self.class.new(INVERSE[@value])
-  end
-
-  def win?
-    @value == :win
-  end
-
-  def draw?
-    @value == :draw
-  end
-
-  def loss?
-    @value == :loss
+  def add_match(home, away, result)
+    @teams[home].send((result.to_s + '!').to_sym)
+    @teams[away].send((INVERSE[result].to_s + '!').to_sym)
   end
 end
 
-class Results
-  attr_reader :name
+class TableFormatter
+  attr_reader :teams
+
+  ROW_FORMATTER = "%-30s | %2s | %2s | %2s | %2s | %2s\n".freeze
+  HEADER = "Team                           | MP |  W |  D |  L |  P\n".freeze
+
+  def initialize(teams)
+    @teams = teams
+  end
+
+  def display
+    [HEADER, *teams.sort.map { |team| show(team) }].join
+  end
+
+  def show(team)
+    format(ROW_FORMATTER, *team.row_list).to_s
+  end
+end
+
+class Team
+  attr_reader :name, :wins, :draws, :loses
+
   def initialize(name)
-    @results = []
     @name = name
+    @wins = @loses = @draws = 0
   end
 
   def points
     wins * 3 + draws
   end
 
-  def loses
-    @results.select(&:loss?).count
-  end
-
-  def draws
-    @results.select(&:draw?).count
-  end
-
-  def wins
-    @results.select(&:win?).count
-  end
-
   def matches
-    @results.count
+    wins + draws + loses
   end
 
-  def add(result)
-    @results << result
+  def win!
+    @wins += 1
+  end
+
+  def draw!
+    @draws += 1
+  end
+
+  def loss!
+    @loses += 1
   end
 
   def row_list
