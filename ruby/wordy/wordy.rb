1292a5c08476c827ece99a45cdb6075b8b683cf5
Murilo Vasconcelos <muriloime@gmail.com>
2020-04-29 20:04:24 -0300

    remove eval

--- ruby/wordy/wordy.rb
+++ ruby/wordy/wordy.rb
@@ -10,15 +10,19 @@ class WordProblem
     @question = question.gsub(Regexp.union(OPERATIONS.keys), OPERATIONS)
   end
 
+  def eval_binary_expression(operand1, operator, operand2)
+    operand1.send(operator, operand2)
+  end
+
   def answer
-    raise ArgumentError unless @question !~ /[a-zA-Z]/
+    raise ArgumentError if /[a-zA-Z]/.match?(@question)
 
     operands = @question.scan(%r{[\d\+\-\*/]+})
 
     operands[1..-1]
       .each_slice(2)
       .inject(operands[0]) do |last_value, (operator, operand)|
-      eval [last_value, operator, operand].join
+      eval_binary_expression(last_value.to_i, operator, operand.to_i)
     end
   end
 end
